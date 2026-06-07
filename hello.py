0dd072112401b1c42a0a0273b713e772b49a60eb
Redip Biswas <redipbiswas8@gmail.com>
2026-06-08 01:12:44 +0600

    Python Code

--- hello.py
+++ hello.py
@@ -3,3 +3,21 @@ a=20
 b=30
 c=a+b
 print("The sum of a and b is:",c)
+if 10<100:
+    print ("10 is less than 100")
+    ##Statements 
+    print("America is a great country"); print("I love programming"); print("Python is a great language")
+    #Python Output / Print
+    print('Brazil is a great country',end=' in South America,')
+    print('I love Neymar',end=' and I love watching football\n')
+    print(2026)
+    print(20+65)
+    print(100-900)
+    print(20*96)
+    print(100/4)
+    print("I am" ,25 ,"years old")
+    # Comments in Python
+    '''This is a single line comment'''
+    """This is a multi-line comment
+    It can span multiple lines
+    and is often used for documentation purposes"""
