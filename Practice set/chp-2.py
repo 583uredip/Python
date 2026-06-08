9e09ac51b113dcc0d16edc57ac724f6310326f57
Redip Biswas <redipbiswas8@gmail.com>
2026-06-08 16:40:09 +0600

    Practice Question

---
+++ Practice set/chp-2.py
@@ -0,0 +1,18 @@
+#Question 1: Addition of two numbers
+a=20
+b=10
+print("Addition Answer is",a+b)
+#Question 2: Remainder when a is divided by b
+a=27
+b=100
+print("Remainder When a is divided by b is",a%b)
+#Question 3:type of variable x and b
+x="I Support Brazil Football Team"
+print(type(x))
+b=input("Enter The Value of b: ")
+print(type(b))
+print("Value of b is",b)
+
+a=int(input("Enter The Value of a: "))
+b=int(input("Enter The Value of b: "))
+print("a is greater than b:",a>b)
