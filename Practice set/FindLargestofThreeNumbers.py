6671eafadd43cff02666edcc99939a7b7f3076d6
583uredip <redipbiswas8@gmail.com>
2026-06-21 19:52:18 +0600

    Practice set

---
+++ Practice set/FindLargestofThreeNumbers.py
@@ -0,0 +1,9 @@
+a=int(input("Enter Number 1:"))
+b=int(input("Enter Number 2:"))
+c=int(input("Enter Number 3:"))
+if a>=b and a>=c:
+   print("largest Number Is:",a)
+elif b>=c and b>=c:
+  print("largest Number Is:",b)
+else:
+    print("largest Number Is:",c)
